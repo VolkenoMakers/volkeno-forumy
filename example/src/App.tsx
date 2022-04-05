d52e90759b7f832531440dfa6220ee76b7aa57e3
@falllifa160 <falllifa160@gmail.com>
2022-04-05 11:29:43 +0000

    fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import 'volkeno-forumy/dist/index.css'
-import { ExampleComponent } from '../../dist'
+import { ExampleComponent } from 'volkeno-forumy'
 
 const App = () => {
   return <ExampleComponent text="Hello volkeno forumy 😄" />
