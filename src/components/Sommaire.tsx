eb71d67999a6906da1fba6c51c9eec7fd3431354
Paul Gomis <gomispapaul6@gmail.com>
2022-04-06 16:58:16 +0000

    intégration sommaire

--- src/components/Sommaire.tsx
+++ src/components/Sommaire.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import styles from '../styles.module.css'
-// import avatar from './avatar.jpeg'
+import { DatasForum } from './DatasForum';
+import { BsChat } from "react-icons/bs";
+import { IoReturnDownBackOutline } from "react-icons/io5";
+// import avatar from './img/avatar.png'
 
 // interface Props {
 //     text: string
 // }
 
 function Sommaire() {
+
+  console.log('data forum',DatasForum)
+
     return (
         <div className="component-liste-projet">
           {/* <FrontHeaderV2 /> */}
@@ -58,120 +64,117 @@ function Sommaire() {
                           getForumData(item);
                         }}
                       > */}
-                        
-                        <div className={
-                            // card card-body 
-                            styles.forumCardSommaire 
-                            // styles.forumCardSommaireFixWidth
-                            }>
-                          <div className={styles.cardSommaireDateAjout}>
-                            {/* {moment(item?.created_at).format("DD/MM/YYYY")} */} date
-                          </div>
-                          <div className={styles.sommaireAvatarTitre}>
-                            <div className="">
-                              <div className="p1">
-                                <img
-                                //   src={
-                                //       !item?.author?.avatar ||
-                                //         item?.author?.avatar?.includes(
-                                //             "default.png"
-                                //         )
-                                //         ? ProjetImg
-                                //         : baseUrl +
-                                //         item?.author?.avatar
-                                //   }
-                                // src={avatar}
-                                  alt="user-sept"
-                                  className="img-sommaire-forum"
-                                />
-                                img
-                              </div>
-                            </div>
-                            <div className="">
-                              <h3 className={
-                                //   card-title 
-                                  styles.titreSommaireForum 
-                                //   pt-md-2
-                                  }>
-                                {/* {item?.titre} */} titre
-                              </h3>
-                              <div className={styles.forumSommaireAuteurCard}>
-                                {/* <IoReturnDownBackOutline className="mr-1" /> */} icone
-                                Par{" "}
-                                {/* {item?.author?.prenom !== undefined ||
-                                item?.author?.nom !== undefined
-                                  ? item?.author?.prenom + " " + item?.author?.nom
-                                  : "Anonyme"} */} autheur
-                              </div>
-                            </div>
-                          </div>
-                          <div className="row">
-                            <div className="col-lg-9 pt-3">
-                              <p className={
-                                //   card-text 
-                                  styles.textSommaireForum
-                                  }>
-                                {/* {item?.contenu} */} 
 
-                                Le vélo est un moyen de transport silencieux et écologique. C'est un engin qui ne présente pas beaucoup de danger. D'une part, le vélo ne nous fait pas respirer les vapeurs d'essence, mais les vapeurs du matin et du soir. D'autre part, il se moque des règlements, il ignore les interdits.
-
-                              </p>
-                            </div>
-                            <div className="col-lg-3">
-                              <div className={styles.sommaireForumUserChat}>
-                                {/* {item?.user_comments
-                                  .slice(0, 4)
-                                  .map((comment, index) => (
+                      {
+                        DatasForum.length > 0  ? (
+                          DatasForum.map((item) => {
+                            return(
+                              <div className={
+                                // card card-body 
+                                styles.forumCardSommaire 
+                                // styles.forumCardSommaireFixWidth
+                                } key={item?.id}>
+                              <div className={styles.cardSommaireDateAjout}>
+                                {item?.created_at}
+                              </div>
+                              <div className={styles.sommaireAvatarTitre}>
+                                <div className="">
+                                  <div className="p1">
                                     <img
-                                      src={
-                                        !comment?.avatar ||
-                                        comment?.avatar?.includes(
-                                          "default.png"
-                                        )
-                                          ? ProjetImg
-                                          : baseUrl + comment?.avatar
-                                      }
-                                      key={comment?.id}
+                                    //   src={
+                                    //       !item?.author?.avatar ||
+                                    //         item?.author?.avatar?.includes(
+                                    //             "default.png"
+                                    //         )
+                                    //         ? ProjetImg
+                                    //         : baseUrl +
+                                    //         item?.author?.avatar
+                                    //   }
+                                      // src={item?.author?.avatar}
+                                      src={item?.author?.avatar === '/mediafiles/avatars/default.png' ? `https://ui-avatars.com/api/?name=${item?.author?.fullname}` : `${item?.author?.avatar}`}
                                       alt="user-avatar"
-                                      className={`img-sommaire-forum-user-chat div-sommaire-forum-user-chat${
-                                        index + 1
-                                      }`}
+                                      className={styles.imgSommaireForum}
                                     />
-                                  ))} */} img
-                                <div className={
-                                    // styles.imgSommaireForumUserChat 
-                                    styles.divSommaireForumUserChat5
-                                    }>
-                                  <div className={styles.troisPoints}>
-                                  {/* {`
-                                        ${item?.user_comments.length > 4 ?  item?.user_comments.length - 4 + "+"  : "..."}
-                                    `} */} ...
+                                  </div>
+                                </div>
+                                <div className="">
+                                  <h3 className={
+                                    //   card-title 
+                                      styles.titreSommaireForum 
+                                    //   pt-md-2
+                                      }>
+                                    {item?.titre} 
+                                  </h3>
+                                  <div className={styles.forumSommaireAuteurCard}>
+                                    <IoReturnDownBackOutline className="mr-1" /> 
+                                    Par{" "}
+                                    {item?.author?.prenom !== undefined ||
+                                    item?.author?.nom !== undefined
+                                      ? item?.author?.prenom + " " + item?.author?.nom
+                                      : "Anonyme"}
                                   </div>
                                 </div>
                               </div>
-                              <div className={styles.divNombreCommentaireSommaireForum}>
-                                <p className={styles.nombreCommentaireSommaireForum}>
-                                  {/* <BsChat className="mr-1" />{" "} */}icone
-                                  {/* {item?.comments.length} Commentaires */} Commentaires
-                                </p>
+                              <div className="row">
+                                <div className="col-lg-9 pt-3">
+                                  <p className={
+                                    //   card-text 
+                                      styles.textSommaireForum
+                                      }>
+                                    {item?.contenu} 
+                                  </p>
+                                </div>
+                                <div className="col-lg-3">
+                                  <div className={styles.sommaireForumUserChat}>
+                                    {/* {item?.user_comments
+                                      .slice(0, 4)
+                                      .map((comment, index) => (
+                                        <img
+                                          src={comment?.user?.avatar === '/mediafiles/avatars/default.png' ? `https://ui-avatars.com/api/?name=${comment?.user?.fullname}` : `${comment?.user?.avatar}`}
+                                          key={comment?.id}
+                                          alt="user-avatar"
+                                          className={`img-sommaire-forum-user-chat div-sommaire-forum-user-chat${
+                                            index + 1
+                                          }`}
+                                        />
+                                      ))}  */}
+                                      {
+                                        console.log( 'user comment',item?.user_comments)
+                                      }
+                                    <div className={
+                                        // styles.imgSommaireForumUserChat 
+                                        styles.divSommaireForumUserChat5
+                                        }>
+                                      <div className={styles.troisPoints}>
+                                      {/* {`
+                                            ${item?.user_comments.length > 4 ?  item?.user_comments.length - 4 + "+"  : "..."}
+                                        `} ... */}
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className={styles.divNombreCommentaireSommaireForum}>
+                                    <p className={styles.nombreCommentaireSommaireForum}>
+                                      <BsChat className="mr-1" />{" "}
+                                      {item?.comments.length} Commentaires
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            )
+                          })
+                        ) : (
+                          <div className="card-contacts-main-content">
+                            <div className="row div-card">
+                              <div className="col-12">
+                                <h4 className="admin-contacts-title text-center">
+                                  Pas de sujet de discussion
+                                </h4>
                               </div>
                             </div>
                           </div>
-                        </div>
-                      {/* </NavLink> */}
-                    {/* );
-                  })
-                ) : ( */}
-                  {/* <div className="card-contacts-main-content">
-                    <div className="row div-card">
-                      <div className="col-12">
-                        <h4 className="admin-contacts-title text-center">
-                          Pas de sujet de discussion
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                )} */}
+                        )
+                      }
               </div>
             </div>
             </div>
