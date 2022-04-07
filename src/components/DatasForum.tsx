21360c64ef452f4c5f433099640aa8c5de6bb35d
@falllifa160 <falllifa160@gmail.com>
2022-04-07 09:29:21 +0000

    pull dev

--- src/components/DatasForum.tsx
+++ src/components/DatasForum.tsx
@@ -1,119 +1,120 @@
-
 export const DatasForum = [
-    {
-        titre: "Quels sont les avantages d'utiliser le vélo comme moyen de transport ?",
-        contenu: " Le vélo est un moyen de transport silencieux et écologique. C'est un engin qui ne présente pas beaucoup de danger. D'une part, le vélo ne nous fait pas respirer les vapeurs d'essence, mais les vapeurs du matin et du soir. D'autre part, il se moque des règlements, il ignore les interdits.",
-        id: 1,
-        created_at: "6/04/2022",
-        author: {
-            prenom: "Joël ",
-            nom: "Gomis",
-            // avatar: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kissclipart.com%2Fgsmnet-ro-clipart-computer-icons-user-avatar-064hvl%2F&psig=AOvVaw16-m3QehDhym_BJ_eY5I-r&ust=1649339519718000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCLD3w9PK__YCFQAAAAAdAAAAABAU"
-            avatar: '/mediafiles/avatars/default.png',
-            fullname: 'JG'
-        },
-        comments: [
-            {
-                id: 1,
-                user: {
-                    prenom: "Sadio",
-                    nom: "Sanghare",
-                    avatar: '/mediafiles/avatars/default.png',
-                },
-                text: "Reponse 1",
-                created_at: "6/04/2022",
-                reponse_commentaire: [
-                {
-                    user: "Sadio",
-                    user_avatar: "image3",
-                    contenu: "Reponse 3",
-                }
-                ]
-            },
-            {
-                id: 2,
-                user: {
-                    prenom: "Sadio",
-                    nom: "Sangh",
-                    avatar: '/mediafiles/avatars/default.png',
-                },
-                text: "Reponse 2",
-                created_at: "6/04/2022",
-                reponse_commentaire: [
-                {
-                    user: "Sadio",
-                    user_avatar: "image3",
-                    contenu: "Reponse 3",
-                }
-                ]
-            }
-        ],
-        user_comments: [
-            {   
-                id: 1,
-                user: {
-                    prenom: "Sadio",
-                    nom: "Sanghare",
-                    avatar: '/mediafiles/avatars/default.png',
-                    fullname: 'SS'
-                },
-            },
-            {   
-                id: 2,
-                user: {
-                    prenom: "Paul",
-                    nom: "Gomis",
-                    avatar: '/mediafiles/avatars/default.png',
-                    fullname: 'PG'
-                },
-            },
-        ]
-
+  {
+    titre:
+      "Quels sont les avantages d'utiliser le vélo comme moyen de transport ?",
+    contenu:
+      " Le vélo est un moyen de transport silencieux et écologique. C'est un engin qui ne présente pas beaucoup de danger. D'une part, le vélo ne nous fait pas respirer les vapeurs d'essence, mais les vapeurs du matin et du soir. D'autre part, il se moque des règlements, il ignore les interdits.",
+    id: 1,
+    created_at: '6/04/2022',
+    author: {
+      prenom: 'Joël ',
+      nom: 'Gomis',
+      // avatar: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kissclipart.com%2Fgsmnet-ro-clipart-computer-icons-user-avatar-064hvl%2F&psig=AOvVaw16-m3QehDhym_BJ_eY5I-r&ust=1649339519718000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCLD3w9PK__YCFQAAAAAdAAAAABAU"
+      avatar: '/mediafiles/avatars/default.png',
+      fullname: 'JG'
     },
-    {
-        titre: "Quels sont les arguments du travail ?",
-        contenu: "Il éloigne l'ennui, le vice et le besoin. Il humanise par le lien social qu'il crée et maintient nos capacités intellectuelles en éveil. Il redresse le perverti, moralise le dévoyé, rend l'homme vertueux.",
-        id: 2,
-        created_at: "6/04/2022",
-        author: {
-            prenom: "Moussa",
-            nom: "Fall",
-            avatar: '/mediafiles/avatars/default.png',
-            fullname: 'MF'
+    comments: [
+      {
+        id: 1,
+        user: {
+          prenom: 'Sadio',
+          nom: 'Sanghare',
+          avatar: '/mediafiles/avatars/default.png'
         },
-        comments: [
-            {
-                user: {
-                    prenom: "Paul",
-                    nom: "Gomis",
-                    avatar: '/mediafiles/avatars/default.png',
-                },
-                text: "Reponse 1",
-                created_at: "6/04/2022",
-                reponse_commentaire: [
-                {
-                    user: "Sadio",
-                    user_avatar: "image3",
-                    contenu: "Reponse 3",
-                }
-                ]
-            },
-            {
-                user: {
-                    prenom: "Sadio",
-                    nom: "Sangh",
-                    avatar: '/mediafiles/avatars/default.png',
-                },
-                text: "Reponse 2",
-                created_at: "6/04/2022",
-                reponse_commentaire: [
-                {
-                    user: "Sadio",
-                    user_avatar: "image3",
-                    contenu: "Reponse 3",
-                }
-                ]
-            }
+        text: 'Reponse 1',
+        created_at: '6/04/2022',
+        reponse_commentaire: [
+          {
+            user: 'Sadio',
+            user_avatar: 'image3',
+            contenu: 'Reponse 3'
+          }
         ]
-    }
+      },
+      {
+        id: 2,
+        user: {
+          prenom: 'Sadio',
+          nom: 'Sangh',
+          avatar: '/mediafiles/avatars/default.png'
+        },
+        text: 'Reponse 2',
+        created_at: '6/04/2022',
+        reponse_commentaire: [
+          {
+            user: 'Sadio',
+            user_avatar: 'image3',
+            contenu: 'Reponse 3'
+          }
+        ]
+      }
+    ],
+    user_comments: [
+      {
+        id: 1,
+        user: {
+          prenom: 'Sadio',
+          nom: 'Sanghare',
+          avatar: '/mediafiles/avatars/default.png',
+          fullname: 'SS'
+        }
+      },
+      {
+        id: 2,
+        user: {
+          prenom: 'Paul',
+          nom: 'Gomis',
+          avatar: '/mediafiles/avatars/default.png',
+          fullname: 'PG'
+        }
+      }
+    ]
+  },
+  {
+    titre: 'Quels sont les arguments du travail ?',
+    contenu:
+      "Il éloigne l'ennui, le vice et le besoin. Il humanise par le lien social qu'il crée et maintient nos capacités intellectuelles en éveil. Il redresse le perverti, moralise le dévoyé, rend l'homme vertueux.",
+    id: 2,
+    created_at: '6/04/2022',
+    author: {
+      prenom: 'Moussa',
+      nom: 'Fall',
+      avatar: '/mediafiles/avatars/default.png',
+      fullname: 'MF'
+    },
+    comments: [
+      {
+        user: {
+          prenom: 'Paul',
+          nom: 'Gomis',
+          avatar: '/mediafiles/avatars/default.png'
+        },
+        text: 'Reponse 1',
+        created_at: '6/04/2022',
+        reponse_commentaire: [
+          {
+            user: 'Sadio',
+            user_avatar: 'image3',
+            contenu: 'Reponse 3'
+          }
+        ]
+      },
+      {
+        user: {
+          prenom: 'Sadio',
+          nom: 'Sangh',
+          avatar: '/mediafiles/avatars/default.png'
+        },
+        text: 'Reponse 2',
+        created_at: '6/04/2022',
+        reponse_commentaire: [
+          {
+            user: 'Sadio',
+            user_avatar: 'image3',
+            contenu: 'Reponse 3'
+          }
+        ]
+      }
+    ]
+  }
 ]
--- src/components/Sommaire.tsx
+++ src/components/Sommaire.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from '../styles.module.css'
-import { DatasForum } from './DatasForum';
-import { BsChat } from "react-icons/bs";
-import { IoReturnDownBackOutline } from "react-icons/io5";
+import { DatasForum } from './DatasForum'
+import { BsChat } from 'react-icons/bs'
+import { IoReturnDownBackOutline } from 'react-icons/io5'
 // import avatar from './img/avatar.png'
 
 // interface Props {
@@ -10,25 +10,24 @@ import { IoReturnDownBackOutline } from "react-icons/io5";
 // }
 
 function Sommaire() {
+  console.log('data forum', DatasForum)
 
-  console.log('data forum',DatasForum)
-
-    return (
-        <div className="component-liste-projet">
-          {/* <FrontHeaderV2 /> */}
-          <div className={styles.contentViewStart}>
-            <div className={styles.bannerListeProjet}>
-              <h3 className={styles.titreBannerListeProjet}>
-                De la discussion, jaillit la lumière
-              </h3>
-            </div>
-            <div className={styles.containerCayore}>
-              <div className="row align-items-center" style={{ margin: "3rem 0" }}>
-              <div className="col-12">
-                {/* {hasTokenExpired ||
+  return (
+    <div className='component-liste-projet'>
+      {/* <FrontHeaderV2 /> */}
+      <div className={styles.contentViewStart}>
+        <div className={styles.bannerListeProjet}>
+          <h3 className={styles.titreBannerListeProjet}>
+            De la discussion, jaillit la lumière
+          </h3>
+        </div>
+        <div className={styles.containerCayore}>
+          <div className='row align-items-center' style={{ margin: '3rem 0' }}>
+            <div className='col-12'>
+              {/* {hasTokenExpired ||
                 hasTokenExpired === undefined ||
                 hasTokenExpired === null ? ( */}
-                  {/* <div className="card-contacts-main-content offline-alert-container">
+              {/* <div className="card-contacts-main-content offline-alert-container">
                     <div className="row div-card offline-alert-card">
                       <div className="col-12 offline-alert-col">
                         <div className="admin-contacts-title text-center offline-alert-textual">
@@ -45,15 +44,15 @@ function Sommaire() {
                       </div>
                     </div>
                   </div> */}
-                {/* ) : forum_data.isLoading ? (
+              {/* ) : forum_data.isLoading ? (
                     [...Array(3)].map((item, index) => (
                         <SommaireSkeleton key={index}/>
                     ))
                   
                 ) : forum_data.data.length > 0 ? (
                   forum_data.data.map((item) => { */}
-                    {/* return ( */}
-                      {/* <NavLink
+              {/* return ( */}
+              {/* <NavLink
                         className=""
                         to={{
                           pathname: `/forum-discussion/${item.id}`,
@@ -65,68 +64,79 @@ function Sommaire() {
                         }}
                       > */}
 
-                      {
-                        DatasForum.length > 0  ? (
-                          DatasForum.map((item) => {
-                            return(
-                              <div className={
-                                // card card-body 
-                                styles.forumCardSommaire 
-                                // styles.forumCardSommaireFixWidth
-                                } key={item?.id}>
-                              <div className={styles.cardSommaireDateAjout}>
-                                {item?.created_at}
-                              </div>
-                              <div className={styles.sommaireAvatarTitre}>
-                                <div className="">
-                                  <div className="p1">
-                                    <img
-                                    //   src={
-                                    //       !item?.author?.avatar ||
-                                    //         item?.author?.avatar?.includes(
-                                    //             "default.png"
-                                    //         )
-                                    //         ? ProjetImg
-                                    //         : baseUrl +
-                                    //         item?.author?.avatar
-                                    //   }
-                                      // src={item?.author?.avatar}
-                                      src={item?.author?.avatar === '/mediafiles/avatars/default.png' ? `https://ui-avatars.com/api/?name=${item?.author?.fullname}` : `${item?.author?.avatar}`}
-                                      alt="user-avatar"
-                                      className={styles.imgSommaireForum}
-                                    />
-                                  </div>
-                                </div>
-                                <div className="">
-                                  <h3 className={
-                                    //   card-title 
-                                      styles.titreSommaireForum 
-                                    //   pt-md-2
-                                      }>
-                                    {item?.titre} 
-                                  </h3>
-                                  <div className={styles.forumSommaireAuteurCard}>
-                                    <IoReturnDownBackOutline className="mr-1" /> 
-                                    Par{" "}
-                                    {item?.author?.prenom !== undefined ||
-                                    item?.author?.nom !== undefined
-                                      ? item?.author?.prenom + " " + item?.author?.nom
-                                      : "Anonyme"}
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="row">
-                                <div className="col-lg-9 pt-3">
-                                  <p className={
-                                    //   card-text 
-                                      styles.textSommaireForum
-                                      }>
-                                    {item?.contenu} 
-                                  </p>
-                                </div>
-                                <div className="col-lg-3">
-                                  <div className={styles.sommaireForumUserChat}>
-                                    {/* {item?.user_comments
+              {DatasForum.length > 0 ? (
+                DatasForum.map((item) => {
+                  return (
+                    <div
+                      className={
+                        // card card-body
+                        styles.forumCardSommaire
+                        // styles.forumCardSommaireFixWidth
+                      }
+                      key={item?.id}
+                    >
+                      <div className={styles.cardSommaireDateAjout}>
+                        {item?.created_at}
+                      </div>
+                      <div className={styles.sommaireAvatarTitre}>
+                        <div className=''>
+                          <div className='p1'>
+                            <img
+                              //   src={
+                              //       !item?.author?.avatar ||
+                              //         item?.author?.avatar?.includes(
+                              //             "default.png"
+                              //         )
+                              //         ? ProjetImg
+                              //         : baseUrl +
+                              //         item?.author?.avatar
+                              //   }
+                              // src={item?.author?.avatar}
+                              src={
+                                item?.author?.avatar ===
+                                '/mediafiles/avatars/default.png'
+                                  ? `https://ui-avatars.com/api/?name=${item?.author?.fullname}`
+                                  : `${item?.author?.avatar}`
+                              }
+                              alt='user-avatar'
+                              className={styles.imgSommaireForum}
+                            />
+                          </div>
+                        </div>
+                        <div className=''>
+                          <h3
+                            className={
+                              //   card-title
+                              styles.titreSommaireForum
+                              //   pt-md-2
+                            }
+                          >
+                            {item?.titre}
+                          </h3>
+                          <div className={styles.forumSommaireAuteurCard}>
+                            <IoReturnDownBackOutline className='mr-1' />
+                            Par{' '}
+                            {item?.author?.prenom !== undefined ||
+                            item?.author?.nom !== undefined
+                              ? item?.author?.prenom + ' ' + item?.author?.nom
+                              : 'Anonyme'}
+                          </div>
+                        </div>
+                      </div>
+                      <div className='row'>
+                        <div className='col-lg-9 pt-3'>
+                          <p
+                            className={
+                              //   card-text
+                              styles.textSommaireForum
+                            }
+                          >
+                            {item?.contenu}
+                          </p>
+                        </div>
+                        <div className='col-lg-3'>
+                          <div className={styles.sommaireForumUserChat}>
+                            {/* {item?.user_comments
                                       .slice(0, 4)
                                       .map((comment, index) => (
                                         <img
@@ -138,50 +148,53 @@ function Sommaire() {
                                           }`}
                                         />
                                       ))}  */}
-                                      {
-                                        console.log( 'user comment',item?.user_comments)
-                                      }
-                                    <div className={
-                                        // styles.imgSommaireForumUserChat 
-                                        styles.divSommaireForumUserChat5
-                                        }>
-                                      <div className={styles.troisPoints}>
-                                      {/* {`
+                            {console.log('user comment', item?.user_comments)}
+                            <div
+                              className={
+                                // styles.imgSommaireForumUserChat
+                                styles.divSommaireForumUserChat5
+                              }
+                            >
+                              <div className={styles.troisPoints}>
+                                {/* {`
                                             ${item?.user_comments.length > 4 ?  item?.user_comments.length - 4 + "+"  : "..."}
                                         `} ... */}
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div className={styles.divNombreCommentaireSommaireForum}>
-                                    <p className={styles.nombreCommentaireSommaireForum}>
-                                      <BsChat className="mr-1" />{" "}
-                                      {item?.comments.length} Commentaires
-                                    </p>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                            )
-                          })
-                        ) : (
-                          <div className="card-contacts-main-content">
-                            <div className="row div-card">
-                              <div className="col-12">
-                                <h4 className="admin-contacts-title text-center">
-                                  Pas de sujet de discussion
-                                </h4>
                               </div>
                             </div>
                           </div>
-                        )
-                      }
-              </div>
-            </div>
+                          <div
+                            className={styles.divNombreCommentaireSommaireForum}
+                          >
+                            <p
+                              className={styles.nombreCommentaireSommaireForum}
+                            >
+                              <BsChat className='mr-1' />{' '}
+                              {item?.comments.length} Commentaires
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+              ) : (
+                <div className='card-contacts-main-content'>
+                  <div className='row div-card'>
+                    <div className='col-12'>
+                      <h4 className='admin-contacts-title text-center'>
+                        Pas de sujet de discussion
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
-          {/* <FrontFooterV2 /> */}
         </div>
-      );
+      </div>
+      {/* <FrontFooterV2 /> */}
+    </div>
+  )
 }
 
 export default Sommaire
