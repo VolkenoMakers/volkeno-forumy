4effbd302c74b8ab6068929cb32013e386b26d69
Paul Gomis <gomispapaul6@gmail.com>
2022-04-22 17:07:25 +0000

    ajout de la methode pour tester l'ajout d'un sujet

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,279 +7,175 @@ const App = () => {
 
     
 
-  let datas = [
-    {
-      initialTitle: "Quels sont les avantages d'utiliser le vélo comme moyen de transport ?",
-      initialContent: " Le vélo est un moyen de transport silencieux et écologique. C'est un engin qui ne présente pas beaucoup de danger. D'une part, le vélo ne nous fait pas respirer les vapeurs d'essence, mais les vapeurs du matin et du soir. D'autre part, il se moque des règlements, il ignore les interdits.",
-      id: 1,
-      slug: 'avantage_velo',
-      created_at: "6/04/2022",
-      author: {
-          firstName: "Joël ",
-          lastName: "Gomis",
-          avatar: '/mediafiles/avatars/default.png',
-      },
-      first_level_response: [
-          {
-              id: 1,
-              slug: 'initial_message_slug',
-              user: {
-                  firstName: "Sadio",
-                  lastName: "Sanghare",
-                  avatar: '/mediafiles/avatars/default.png',
-              },
-              content: "Comment 1",
-              created_at: "6/04/2022",
-              second_level_response: [
-              {
-                id: 1,
-                slug: 'first_level_message_slug',
-                  user: {
-                      firstName: "Ndeye",
-                      lastName: "Faye",
-                      avatar: '/mediafiles/avatars/default.png',
-                  },
-                  content: "Reponse 7 deus",
-              }
-              ]
-          },
-          {
-              id: 2,
-              slug: 'initial_message_slug',
-              user: {
-                  firstName: "Amadou",
-                  lastName: "Sall",
-                  avatar: '/mediafiles/avatars/default.png',
-              },
-              content: "Reponse 2",
-              created_at: "6/04/2022",
-              second_level_response: [
-              {
-                  id: 1,
-                  slug: 'first_level_message_slug',
-                  user: {
-                      firstName: "Fatou",
-                      lastName: "Samb",
-                      avatar: '/mediafiles/avatars/default.png',
-                  },
-                  content: "Reponse 10",
-              },
-              {
-                  id: 2,
-                  slug: 'first_level_message_slug',
-                  user: {
-                      firstName: "Modou",
-                      lastName: "Ndiaye",
-                      avatar: '/mediafiles/avatars/default.png',
-                  },
-                  content: "Reponse 18",
-              }
-            ]
-          },
-          {
-              id: 3,
-              slug: 'initial_message_slug',
-              user: {
-                  firstName: "Aïssatou",
-                  lastName: "Diallo",
-                  avatar: '/mediafiles/avatars/default.png',
-              },
-              content: "Reponse 12",
-              created_at: "21/04/2022",
-              second_level_response: [
-              {
-                  id: 1,
-                  slug: 'first_level_message_slug',
-                  user: {
-                      firstName: "Fatou",
-                      lastName: "Samb",
-                      avatar: '/mediafiles/avatars/default.png',
-                  },
-                  content: "Reponse 10",
-              }
-              ]
-          }
-      ],
-      user_comments: [
-          {   
-              id: 1,
-              user: {
-                  firstName: "Sadio",
-                  lastName: "Sanghare",
-                  avatar: '/mediafiles/avatars/default.png',
-              },
-          },
-          {   
-              id: 2,
-              user: {
-                  firstName: "Amadou",
-                  lastName: "Sall",
-                  avatar: '/mediafiles/avatars/default.png',
-              },
-          },
-          {   
-              id: 3,
-              user: {
-                  firstName: "Aïssatou",
-                  lastName: "Diallo",
-                  avatar: '/mediafiles/avatars/default.png',
-              },
-          }
-      ]
+//   let datas = [
+//     {
+//       initialTitle: "Quels sont les avantages d'utiliser le vélo comme moyen de transport ?",
+//       initialContent: " Le vélo est un moyen de transport silencieux et écologique. C'est un engin qui ne présente pas beaucoup de danger. D'une part, le vélo ne nous fait pas respirer les vapeurs d'essence, mais les vapeurs du matin et du soir. D'autre part, il se moque des règlements, il ignore les interdits.",
+//       id: 1,
+//       slug: 'avantage_velo',
+//       created_at: "6/04/2022",
+//       author: {
+//           firstName: "Joël ",
+//           lastName: "Gomis",
+//           avatar: '/mediafiles/avatars/default.png',
+//       },
+//       first_level_response: [
+//           {
+//               id: 1,
+//               slug: 'initial_message_slug',
+//               user: {
+//                   firstName: "Sadio",
+//                   lastName: "Sanghare",
+//                   avatar: '/mediafiles/avatars/default.png',
+//               },
+//               content: "Comment 1",
+//               created_at: "6/04/2022",
+//               second_level_response: [
+//               {
+//                 id: 1,
+//                 slug: 'first_level_message_slug',
+//                   user: {
+//                       firstName: "Ndeye",
+//                       lastName: "Faye",
+//                       avatar: '/mediafiles/avatars/default.png',
+//                   },
+//                   content: "Reponse 7 deus",
+//               }
+//               ]
+//           }
+//   ]
 
-    },
-    {
-        initialTitle: "Quels sont les arguments du travail ?",
-        initialContent: "Il éloigne l'ennui, le vice et le besoin. Il humanise par le lien social qu'il crée et maintient nos capacités intellectuelles en éveil. Il redresse le perverti, moralise le dévoyé, rend l'homme vertueux.",
-        id: 2,
-        slug: 'arguments_travail',
-        created_at: "6/04/2022",
-        author: {
-            firstName: "Moussa",
-            lastName: "Fall",
-            avatar: '/mediafiles/avatars/default.png',
-        },
-        first_level_response: [
-            {
-                id: 1,
-                slug: 'initial_message_slug',
-                user: {
-                    firstName: "Paul",
-                    lastName: "Gomis",
-                    avatar: '/mediafiles/avatars/default.png',
-                },
-                content: "Reponse 1",
-                created_at: "6/04/2022",
-                second_level_response: [
-                {
-                    id: 1,
-                    slug: 'first_level_message_slug',
-                    user: {
-                        firstName: "Alpha",
-                        lastName: "Diallo",
-                        avatar: '/mediafiles/avatars/default.png',
-                    },
-                    content: "Reponse 3",
-                }
-                ]
-            },
-            {
-                id: 2,
-                slug: 'initial_message_slug',
-                user: {
-                    firstName: "Sadio",
-                    lastName: "Sangh",
-                    avatar: '/mediafiles/avatars/default.png',
-                },
-                content: "Reponse 2",
-                created_at: "6/04/2022",
-                second_level_response: [
-                {
-                    id: 1,
-                    slug: 'first_level_message_slug',
-                    user: {
-                        firstName: "Cheikh",
-                        lastName: "Dieng",
-                        avatar: '/mediafiles/avatars/default.png',
-                    },
-                    content: "Reponse 6",
-                }
-                ]
-            }
-        ],
-        user_comments: [
-            {   
-                id: 1,
-                user: {
-                    firstName: "Sadio",
-                    lastName: "Sangh",
-                    avatar: '/mediafiles/avatars/default.png',
-                },
-            },
-            {   
-                id: 2,
-                user: {
-                    firstName: "Paul",
-                    lastName: "Gomis",
-                    avatar: '/mediafiles/avatars/default.png',
-                },
-            }
-        ]
-
-    }
-  ]
-
-  const [Datas, setData] = useState(datas)
+  const [Datas, setDatas] = useState<string[]>([])
 
 //======================= AJOUT SUJET ===================================
 
   const [initialContent, setInitialContent] = useState('')
   const [initialTitle, setInitialTitle] = useState('')
-  let id = 3
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+
+  function generateUniqueID() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 5; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+  }
+
+  const resetForm = () => {
+    (document.getElementById('add_msg_form') as HTMLFormElement).reset();
+  }
 
     const onAdd = (e:any) => {
         e.preventDefault()
         if(initialTitle.trim().length > 0 &&  initialContent.trim().length > 0){
-
-            let author = {
-                "firstName": "Paul",
-                "lastName": "Gomis",
-                "avatar": "/mediafiles/avatars/default.png"
-            }
-            let response = [{
-
-                id: 1,
-                slug: 'initial_message_slug',
-                user: {
-                    firstName: "Sadio",
-                    lastName: "Sanghare",
-                    avatar: '/mediafiles/avatars/default.png',
-                },
-                content: "Comment 1",
-                created_at: "6/04/2022",
-                second_level_response: [
-                    {
-                        id: 1,
-                        slug: 'first_level_message_slug',
-                        user: {
-                            firstName: "Ndeye",
-                            lastName: "Faye",
-                            avatar: '/mediafiles/avatars/default.png',
-                        },
-                        content: "Reponse 7 deus",
-                    }
-                ]
-                
-            }]
-
-            let newSujet = {
+            
+            let fields: any = {
                 initialTitle: initialTitle,
                 initialContent: initialContent,
-                id: id,
-                created_at: "22/04/2022",
-                author: author,
-                slug: 'test_slug',
-                first_level_response: response
+                id: generateUniqueID(),
+                slug: generateUniqueID(),
+                created_at: '22/04/2022',
+                author: {
+                    firstName: firstName,
+                    lastName: lastName,
+                    avatar: '/mediafiles/avatars/default.png',
+                }
             }
 
-            let newDonnee:any = [...datas, newSujet]
-            setData(newDonnee)
-            console.log('new data',newDonnee)
+            setDatas([...Datas, fields])
+            resetForm()
+            setInitialContent('')
+            setInitialTitle('')
+            setFirstName('')
+            setLastName('')
+            
+            
         }
     }
-
-  
-
   
   return (
       <div className='forum-container'>
-          <VolkenoForumy data={Datas}  />
+          <VolkenoForumy Datas={Datas}  />
+
+          {/* ======================== AJOUT SUJET ========================= */}
 
             <div className="modal fade" id="exampleModal"  aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div className="modal-dialog">
                 <div className="modal-content">
                   <div className="modal-header">
                     <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <form id='add_msg_form'> 
+                  <div className="modal-body">
+
+                  
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlInputdeux">First name</label>
+                      <input type="text" className="form-control" id="exampleFormControlInputdeux" placeholder="FirstName"
+                      value={firstName}
+                      onChange={(e)=>{
+                        setFirstName(e.target.value)
+                      }}
+                       />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlInputtrois">Last name</label>
+                      <input type="text" className="form-control" id="exampleFormControlInputtrois" placeholder="LastName"
+                      value={lastName}
+                      onChange={(e)=>{
+                        setLastName(e.target.value)
+                      }}
+                       />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="examphtmlFor=''mControlInput1">Titre</label>
+                      <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Titre du sujet"
+                      value={initialTitle}
+                      onChange={(e)=>{
+                        setInitialTitle(e.target.value)
+                      }}
+                       />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlTextarea1">Contenu</label>
+                      <textarea className="form-control" id="exampleFormControlTextarea1" rows={3}
+                       value={initialContent}
+                       onChange={(e)=>{
+                        setInitialContent(e.target.value)
+                       }}
+                      ></textarea>
+                    </div>
+                  
+                    
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                    <button type="submit" className="btn btn-primary"
+                        onClick={onAdd}
+                        data-dismiss="modal"
+                    >Enrégistrer</button>
+                  </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+
+            {/* ============================ AJOUT COMMENT ============================ */}
+
+
+            {/* <div className="modal fade" id="exampleModal2"  aria-labelledby="exampleModalLabel2" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel2">Modal title</h5>
                     <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
@@ -299,9 +195,9 @@ const App = () => {
                     <div className="form-group">
                       <label htmlFor="exampleFormControlTextarea1">Contenu</label>
                       <textarea className="form-control" id="exampleFormControlTextarea1" rows={3}
-                       value={initialContent}
+                       value={responseContent}
                        onChange={(e)=>{
-                        setInitialContent(e.target.value)
+                        setResponseContent(e.target.value)
                        }}
                       ></textarea>
                     </div>
@@ -311,13 +207,13 @@ const App = () => {
                   <div className="modal-footer">
                     <button type="button" className="btn btn-secondary" data-dismiss="modal">Annuler</button>
                     <button type="submit" className="btn btn-primary"
-                        onClick={onAdd}
+                        onClick={onAddComment}
                         data-dismiss="modal"
                     >Enrégistrer</button>
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
 
 
       </div>
