3253c59fae51ec4190989e64b4ec02c73b45aaf7
Paul Gomis <gomispapaul6@gmail.com>
2022-04-22 08:51:27 +0000

    ajout de modale pour ajouter un sujet

--- src/components/Sommaire.tsx
+++ src/components/Sommaire.tsx
@@ -15,6 +15,33 @@ const Sommaire: React.FC<DatasType> = ({ Datas }): JSX.Element => {
               {
                   <SommaireItem Datas={Datas}  />
               } 
+
+
+
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+              Ajouter un sujet
+            </button>
+
+            <div className="modal fade" id="exampleModal"  aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    ...
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                    <button type="button" className="btn btn-primary">Enrégistrer</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
           </div>
         </div>
