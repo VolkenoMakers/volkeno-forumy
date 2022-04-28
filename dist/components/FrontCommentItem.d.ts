51c64ea9dec16da7a8ca6cde3dbfa2f2452a06c1
Sadiosan <knfree99@gmail.com>
2022-04-28 17:48:11 +0000

    fix

--- dist/components/FrontCommentItem.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-declare const FrontCommentItem: React.FC<any>;
-export default FrontCommentItem;
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface ForumProps {
-    Datas: any;
-    DatasUserSession: any;
-}
-export declare const VolkenoForumy: ({ ...props }: ForumProps) => JSX.Element;
-export {};
