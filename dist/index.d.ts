c6c8915ebc1fbee4211c94d571fbe0fd84751303
Sadiosan <knfree99@gmail.com>
2022-04-29 17:13:00 +0000

    fix git cache

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface ForumProps {
-    Datas: any;
-    DatasUserSession: any;
-    hasThirdLevel?: any;
-}
-export declare const VolkenoForumy: ({ ...props }: ForumProps) => JSX.Element;
-export {};
